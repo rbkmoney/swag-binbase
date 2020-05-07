57820feb1ccb83bfbff67149f58c674344eda80f
Kehitt <devstealth@gmail.com>
2020-05-07 15:22:55 +0300

    Bring the build script back

---
+++ build.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+var Path = require('path');
+
+var TARGET_DIR = 'web_deploy'
+if (process.argv[2]) {
+    TARGET_DIR = process.argv[2]
+}
+
+require('shelljs/global');
+set('-e');
+
+cp('-R', 'web/*', TARGET_DIR + '/');
+
+exec('npm run openapi bundle -- -o ' + TARGET_DIR + ' --ext json');
+exec('npm run openapi bundle -- -o ' + TARGET_DIR + ' --ext yaml');
+
+var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui-dist'));
+rm('-rf', TARGET_DIR + '/swagger-ui/')
+cp('-R', SWAGGER_UI_DIST, TARGET_DIR + '/swagger-ui/')
+sed('-i', 'https://petstore.swagger.io/v2/swagger.json', '../openapi.json', TARGET_DIR + '/swagger-ui/index.html')
